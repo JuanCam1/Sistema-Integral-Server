b3e43684be9b709ff2d8ccad9d21e7d632c9c887
Juan Camilo Rojas <juancamilor576@gmail.com>
2025-08-07 17:45:08 -0500

    fix: solucionado services y controller state

--- src/modules/state/state.controller.ts
+++ src/modules/state/state.controller.ts
@@ -1,6 +1,11 @@
-import { Controller, Get, HttpStatus } from "@nestjs/common";
+import { Controller, Get, Res } from "@nestjs/common";
 import { StateService } from "./state.service";
 import { ApiOperation } from "@nestjs/swagger";
+import { Response } from "express";
+import { HttpCode } from "src/utils/http-code";
+import { StatusModel } from "types/status.model";
+import { sendResponse } from "src/utils/send-response";
+import { validateError } from "src/utils/validate-error";
 
 @Controller("state")
 export class StateController {
@@ -8,8 +13,18 @@ export class StateController {
 
   @Get()
   @ApiOperation({ summary: "Get all states" })
-  async findAll() {
-    const data = await this.stateService.findAll();
-    return { data, message: "OK", state: "SUCCESS", codeError: HttpStatus.OK };
+  async findAll(@Res() res: Response) {
+    try {
+      const data = await this.stateService.findAll();
+      return sendResponse(
+        data,
+        "State found",
+        StatusModel.SUCCESS,
+        res,
+        HttpCode.OK,
+      );
+    } catch (error) {
+      return validateError(error, res);
+    }
   }
 }
